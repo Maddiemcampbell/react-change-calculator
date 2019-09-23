f2cabfbdbf24cfac0a4043eafe549da3f36cd430
Maddie <maddiemcampbell@gmail.com>
2019-09-23 13:47:17 -0700

    state management on input fields

--- src/app.jsx
+++ src/app.jsx
@@ -3,18 +3,47 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      amountDue : 0,
-      amountReceived : 0,
-      totalChange : 0,
-      twenties : 0,
-      tens : 0,
-      fives : 0,
-      quarters : 0,
-      dimes : 0,
-      nickels : 0,
-      pennies : 0,
-    }
+      this.state = {
+        amountDue : 0,
+        amountReceived : 0,
+        totalChange : 0,
+        twenties : 0,
+        tens : 0,
+        fives : 0,
+        ones: 0,
+        quarters : 0,
+        dimes : 0,
+        nickels : 0,
+        pennies : 0,
+    } 
+      this.calculate=this.calculate.bind(this);
+  }
+
+  handleInputChange (e) {
+    this.setState({ [e.target.name] : e.target.value});
+  }
+
+  calculate (e) {
+    e.preventDefault();
+    var received = document.getElementById("amount-received").value;
+    var due = document.getElementById("amount-due").value;
+    var change = received - due;
+
+    dollars = Math.floor(change/1);
+    change = (change % 1).toFixed(2);
+    
+    quarters = Math.floor(change/.25);
+    change = (change % .25).toFixed(2);
+
+    dimes = Math.floor(change/.10);
+    change = (change % .10).toFixed(2);
+   
+    nickels = Math.floor(change/.05);
+    change = (change % .05).toFixed(2);
+    
+    pennies = Math.floor(change/.01);
+    change = (change % .01).toFixed(2);
+
   }
 
   render() {
@@ -35,10 +64,10 @@ class App extends Component {
       
         <div id='inputFields'>
           <label>How much is due?</label>
-          <input value={this.state.amountDue} ></input>
+          <input value={this.state.amountDue} onChange={this.handleInputChange} type ='number'></input>
           <label>How much is received?</label>
-          <input value={this.state.amountReceived}></input>
-          <button>Calculate</button>
+          <input value={this.state.amountReceived} onChange={this.handleInputChange} type ='number'></input>
+          <button onClick={this.calculate}>Calculate</button>
         </div>
       
         <div id='output-body'>
