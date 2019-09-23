52dd8acb4143df0dee5ff064e8f94629de1a8ec4
Maddie <maddiemcampbell@gmail.com>
2019-09-23 13:51:49 -0700

    event binding to calculate button works

--- src/app.jsx
+++ src/app.jsx
@@ -25,25 +25,9 @@ class App extends Component {
 
   calculate (e) {
     e.preventDefault();
-    var received = document.getElementById("amount-received").value;
-    var due = document.getElementById("amount-due").value;
-    var change = received - due;
 
-    dollars = Math.floor(change/1);
-    change = (change % 1).toFixed(2);
-    
-    quarters = Math.floor(change/.25);
-    change = (change % .25).toFixed(2);
-
-    dimes = Math.floor(change/.10);
-    change = (change % .10).toFixed(2);
-   
-    nickels = Math.floor(change/.05);
-    change = (change % .05).toFixed(2);
-    
-    pennies = Math.floor(change/.01);
-    change = (change % .01).toFixed(2);
-
+    console.log('event binding to onClick works')
+  
   }
 
   render() {
